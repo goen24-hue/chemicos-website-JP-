130942953fc8c7aec19cb9994ebbfecbedac34f9
goen24-hue <goen24@gmail.com>
2026-07-09 09:34:16 +0900

    Update Korean website copy

--- client/src/pages/chemicos/components/ChemicosCTA.tsx
+++ client/src/pages/chemicos/components/ChemicosCTA.tsx
@@ -49,7 +49,7 @@ export default function ChemicosCTA() {
           className="absolute inset-0 w-full h-full object-cover"
           style={{ filter: "brightness(0.75)" }}
         >
-          <source src="/manus-storage/cta-nature-bg_11e5a4ac.mp4" type="video/mp4" />
+          <source src="/videos/nature.mp4" type="video/mp4" />
         </video>
         <div className="absolute inset-0 bg-black/20" />
 
